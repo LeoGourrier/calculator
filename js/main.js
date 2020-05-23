38b707e9d7406f63c5cf5770497f340ffa2b10dd
LG <leogourrier@gmail.com>
2020-05-23 22:40:45 +0200

    Operators are the last feature left

---
+++ js/main.js
@@ -0,0 +1,159 @@
+function add(a, b) {
+	a = a + b;
+	return a;
+}
+function subtract(a, b) {
+	a = a - b;
+	return a;
+}
+function multiply(a, b) {
+	a = a * b;
+	return a;
+}
+function divide(a, b) {
+	a = a / b;
+	return a;
+}
+//combines functions: add, subtract, multiply, divide
+function operate(operator, num1, num2) {
+	switch (operator) {
+		case '+':
+			return add(num1, num2);
+			break;
+		case '-':
+			return subtract(num1, num2);
+			break;
+		case '*':
+			return multiply(num1, num2);
+			break;
+		case '/':
+			return divide(num1, num2);
+			break;
+		default:
+			return 'error';
+	}
+}
+function populateCalculator() {
+	let calculator = document.querySelector('#calculator');
+	let parts = 3;
+
+	//creates the 3 major sections of the calculator
+	for (let i = 0; i < 3; i++) {
+		let part = document.createElement('div');
+		part.classList = 'parts';
+		part.id = `part${i}`;
+		calculator.appendChild(part);
+	}
+	//creates the screen
+	let screen = document.createElement('div');
+	screen.id = 'screen';
+	screen.innerHTML = `<input id="display" type="text" readonly>`;
+
+	let part0 = document.querySelector('#part0');
+	part0.appendChild(screen);
+	//creates the numbers
+	let part1 = document.querySelector('#part1');
+	for (let i = 1; i < 13; i++) {
+		let number = document.createElement('div');
+		number.classList = 'numbers';
+		if (i == 11) {
+			number.id = `number0`;
+			number.innerHTML = `<h1>0</h1>`;
+		} else {
+			number.id = `number${i}`;
+			if (!(i == 12 || i == 10)) number.innerHTML = `<h1>${i}</h1>`;
+		}
+		part1.appendChild(number);
+	}
+	//creates the operators
+	let operators = [ 'clear', 'add', 'subtract', 'multiply', 'divide', 'equal' ];
+	let operatorSymbols = [ 'cl', '+', '-', 'x', '/', '=' ];
+	let part2 = document.querySelector('#part2');
+	for (let i = 0; i < operators.length; i++) {
+		let operator = document.createElement('div');
+		operator.classList = 'operators';
+		operator.id = `operator${_capitalize(operators[i])}`;
+		operator.innerHTML = `<h1>${operatorSymbols[i]}</h1>`;
+		part2.appendChild(operator);
+	}
+}
+function _capitalize(str) {
+	return str[0].toUpperCase() + str.slice(1, str.length);
+}
+function populateScreen(x) {
+	let display = document.querySelector('#display');
+	tempStorage += x;
+	display.value = tempStorage;
+}
+function clearScreen() {
+	let display = document.querySelector('#display');
+	display.value = '';
+}
+function activateNumbers() {
+	let numbers = Array.from(document.querySelectorAll('.numbers'));
+	numbers = numbers.filter((x) => x.id != 'number10');
+	numbers = numbers.filter((x) => x.id != 'number12');
+	numbers.map((x) =>
+		x.addEventListener('click', function() {
+			//when a number is clicked
+			populateScreen(x.firstElementChild.innerText);
+		})
+	);
+}
+function activateOperators() {
+	let operators = Array.from(document.querySelectorAll('.operators'));
+	operators.map((x) =>
+		x.addEventListener('click', function() {
+			//console.log(x.firstElementChild.innerText);
+			let value = x.firstElementChild.innerText;
+			switch (value) {
+				case 'cl':
+					clearScreen();
+					tempStorage = '';
+					numStorage = [];
+					break;
+				case '+':
+					numStorage.push(tempStorage);
+					numStorage.push(value);
+					clearScreen();
+					tempStorage = '';
+					break;
+				case '-':
+					numStorage.push(tempStorage);
+					numStorage.push(value);
+					clearScreen();
+					tempStorage = '';
+					break;
+				case 'x':
+					numStorage.push(tempStorage);
+					numStorage.push(value);
+					clearScreen();
+					tempStorage = '';
+					break;
+				case '/':
+					numStorage.push(tempStorage);
+					numStorage.push(value);
+					clearScreen();
+					tempStorage = '';
+					break;
+				case '=':
+					break;
+				default:
+			}
+			//move tempStorage to numStorage
+			//add operator to numStorage
+			//clear screen
+			//clear tempStorage
+		})
+	);
+
+	//console.log(operators);
+}
+
+//console.log(operate('/', 4, 2));
+//console.log(_capitalize('no way'));
+let tempStorage = '';
+let numStorage = [];
+populateCalculator();
+activateNumbers();
+activateOperators();
